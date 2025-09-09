3d2333a46e07d6da8fa63c83d82e173ef0d58301
Karl Gaissmaier <karl.gaissmaier@uni-ulm.de>
2025-09-09 20:55:27 +0200

    linter

--- art/setup_test.go
+++ art/setup_test.go
@@ -7,13 +7,8 @@ import (
 const pfxFile = "../testdata/prefixes.txt.gz"
 
 var (
-	prng = common.Prng
-
-	tier1Routes = common.ReadFullTableShuffled(pfxFile)
-
+	tier1Routes   = common.ReadFullTableShuffled(pfxFile)
 	randomRoutes  = common.RandomRealWorldPrefixes(1_000_000)
 	randomRoutes4 = common.RandomRealWorldPrefixes4(1_000_000)
 	randomRoutes6 = common.RandomRealWorldPrefixes6(1_000_000)
-
-	probe = tier1Routes[prng.IntN(len(tier1Routes))]
 )
