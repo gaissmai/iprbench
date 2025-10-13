adc663c6a49512269e532825d2d4c6006a17cb2f
Karl Gaissmaier <karl.gaissmaier@uni-ulm.de>
2025-10-13 23:30:43 +0200

    add bart.Lite and bart.Fast, remove outdated packages

---
+++ fast/update_test.go
@@ -0,0 +1,52 @@
+package main_test
+
+import (
+	"testing"
+
+	"local/iprbench/common"
+
+	"github.com/gaissmai/bart"
+)
+
+func BenchmarkInsertRandomPfxs(b *testing.B) {
+	for _, k := range []int{1_000, 10_000, 100_000, 200_000} {
+		name := common.IntMap[k]
+		randomPfxs := common.RandomRealWorldPrefixes(k)
+
+		b.Run(name, func(b *testing.B) {
+			for b.Loop() {
+				rt := new(bart.Fast[any])
+				for _, route := range randomPfxs {
+					rt.Insert(route, nil)
+				}
+			}
+			b.ReportMetric(float64(b.Elapsed())/float64(k)/float64(b.N), "ns/route")
+			b.ReportMetric(0, "ns/op")
+		})
+	}
+}
+
+func BenchmarkDeleteRandomPfxs(b *testing.B) {
+	for _, k := range []int{1_000, 10_000, 100_000, 200_000} {
+
+		randomPfxs := common.RandomRealWorldPrefixes(k)
+		name := common.IntMap[k]
+
+		b.Run(name, func(b *testing.B) {
+			for b.Loop() {
+				b.StopTimer()
+				rt := new(bart.Fast[any])
+				for _, route := range randomPfxs {
+					rt.Insert(route, nil)
+				}
+				b.StartTimer()
+
+				for _, route := range randomPfxs {
+					rt.Delete(route)
+				}
+			}
+			b.ReportMetric(float64(b.Elapsed())/float64(k)/float64(b.N), "ns/route")
+			b.ReportMetric(0, "ns/op")
+		})
+	}
+}
