bb47fae23dde1b021ec28b1ffbbf0cf9d9e0778b
Karl Gaissmaier <karl.gaissmaier@uni-ulm.de>
2026-09-03 23:59:33 +0200

    benchmark: introduce lookup key rotation

--- kentik-patricia/lpm_test.go
+++ kentik-patricia/lpm_test.go
@@ -10,12 +10,12 @@ import (
 func BenchmarkLpmTier1Pfxs(b *testing.B) {
 	benchmarks := []struct {
 		name string
-		fn   func([]netip.Prefix) netip.Addr
+		fn   func(int, []netip.Prefix) []netip.Addr
 	}{
-		{"RandomMatchIP4", common.MatchIP4},
-		{"RandomMatchIP6", common.MatchIP6},
-		{"RandomMissIP4", common.MissIP4},
-		{"RandomMissIP6", common.MissIP6},
+		{"RandomMatchIP4", common.MatchManyIP4},
+		{"RandomMatchIP6", common.MatchManyIP6},
+		{"RandomMissIP4", common.MissManyIP4},
+		{"RandomMissIP6", common.MissManyIP6},
 	}
 
 	rt := NewTable[any]()
@@ -25,10 +25,14 @@ func BenchmarkLpmTier1Pfxs(b *testing.B) {
 
 	for _, bm := range benchmarks {
 		b.Run(bm.name, func(b *testing.B) {
-			ip := bm.fn(tier1Routes)
+			manyIPs := bm.fn(common.N, tier1Routes)
+
+			i := 0
 			for b.Loop() {
-				rt.Lookup(ip)
+				rt.Lookup(manyIPs[i&common.Mask])
+				i++
 			}
+
 		})
 	}
 }
@@ -36,12 +40,12 @@ func BenchmarkLpmTier1Pfxs(b *testing.B) {
 func BenchmarkLpmRandomPfxs(b *testing.B) {
 	benchmarks := []struct {
 		name string
-		fn   func([]netip.Prefix) netip.Addr
+		fn   func(int, []netip.Prefix) []netip.Addr
 	}{
-		{"RandomMatchIP4", common.MatchIP4},
-		{"RandomMatchIP6", common.MatchIP6},
-		{"RandomMissIP4", common.MissIP4},
-		{"RandomMissIP6", common.MissIP6},
+		{"RandomMatchIP4", common.MatchManyIP4},
+		{"RandomMatchIP6", common.MatchManyIP6},
+		{"RandomMissIP4", common.MissManyIP4},
+		{"RandomMissIP6", common.MissManyIP6},
 	}
 
 	for _, k := range []int{1_000, 10_000, 100_000} {
@@ -53,10 +57,14 @@ func BenchmarkLpmRandomPfxs(b *testing.B) {
 			}
 
 			b.Run(common.IntMap[k]+"/"+bm.name, func(b *testing.B) {
-				ip := bm.fn(randomRoutes[:k]) // get a random matching or missing ip
+				manyIPs := bm.fn(common.N, randomRoutes[:k])
+
+				i := 0
 				for b.Loop() {
-					rt.Lookup(ip)
+					rt.Lookup(manyIPs[i&common.Mask])
+					i++
 				}
+
 			})
 		}
 	}
